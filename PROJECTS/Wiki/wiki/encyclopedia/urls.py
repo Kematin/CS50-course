c73cb4a0d4d2edd48f8c8bb2e2d2efebdb6cf4e5
kematin <kematin@inbox.ru>
2022-09-04 10:41:20 +0500

    create routes to articles, create exception Template not exist error, made the names of the articles links to these very articles

--- PROJECTS/Wiki/wiki/encyclopedia/urls.py
+++ PROJECTS/Wiki/wiki/encyclopedia/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
+from . import util
+list_entries = util.list_entries()
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:article>", views.open_article_page, name="article"),
+    path("wiki/", views.index, name="wiki"),
 ]
--- PROJECTS/Wiki/wiki/encyclopedia/views.py
+++ PROJECTS/Wiki/wiki/encyclopedia/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from django.template.exceptions import TemplateDoesNotExist
 
 from . import util
+list_entries = util.list_entries()
 
 
 def index(request):
-    # util.convert_and_save_html('Django')
+    # util.convert_and_save_html('Mirimo')
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": list_entries
     })
 
+def open_article_page(request, article):
+    if article in list_entries:
+        try:
+            return render(request, f"entries_html/{article}.html")
+        except TemplateDoesNotExist:
+            print(f"[!] Article {article} not found")
+            return render(request, f"error/article_not_found.html")
+
+    else:
+        print(f"[!] Article {article} not found")
+        return render(request, f"error/article_not_found.html")
