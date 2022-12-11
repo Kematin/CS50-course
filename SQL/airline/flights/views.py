92bd28d8d41796216cd8b45a8fda6c90314b2a8c
kematin <kematin@inbox.ru>
2022-12-11 13:27:36 +0500

    display information from db to site

--- SQL/airline/flights/views.py
+++ SQL/airline/flights/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Flight
 
 # Create your views here.
 
-def index(request): ...
+def index(request):
+    context = {
+            "flights": Flight.objects.all()
+        }
+    return render(request, "index.html", context)
