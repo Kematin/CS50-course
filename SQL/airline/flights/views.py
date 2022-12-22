4801b4d808ae930a398e28b7664fdc7c9132d442
kematin <kematin@inbox.ru>
2022-12-22 19:14:21 +0500

    add function: dispaly more information about flights

--- SQL/airline/flights/views.py
+++ SQL/airline/flights/views.py
@@ -9,3 +9,10 @@ def index(request):
             "flights": Flight.objects.all(),
         }
     return render(request, "index.html", context)
+
+def flight(request, flight_id):
+    flight = Flight.objects.all().get(id=flight_id)
+    context = {
+            "flight": flight,
+        }
+    return render(request, "flight.html", context)
