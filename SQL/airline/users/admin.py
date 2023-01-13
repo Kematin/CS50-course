67ee53c83ecf66efda7d413d5e89f676a6dff218
kematin <kematin@inbox.ru>
2023-01-13 17:16:58 +0500

    delete user application

--- SQL/airline/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-class UserView(admin.ModelAdmin):
-    list_display = ("login", "password")    
-
-# Register your models here.
-admin.site.register(User, UserView)
--- SQL/airline/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    login = models.CharField(max_length=64)
-    password = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"{self.login}: {self.password}"
