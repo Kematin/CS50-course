8904bf8b9b8878c1d3055f16c8e875a5b1ee31d5
kematin <kematin@inbox.ru>
2023-01-24 14:33:30 +0500

    add watchlist field to user model

--- PROJECTS/Ebay/commerce/auctions/models.py
+++ PROJECTS/Ebay/commerce/auctions/models.py
@@ -2,10 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    pass
-
-
 class Category(models.Model):
     name = models.CharField(max_length=25)
     
@@ -24,7 +20,11 @@ class Listing(models.Model):
 
     def __str__(self):
         return f"{self.name}: price - {self.cost}"
-    
+
+
+class User(AbstractUser):
+    watchlist = models.ManyToManyField(Listing, blank=True, related_name="watchlist")
+
 
 class Commentary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentary_user")
