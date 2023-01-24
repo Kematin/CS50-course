f1a9c9e351ab736b0aa59ac2be0795a94b03c0d2
kematin <kematin@inbox.ru>
2023-01-24 14:27:45 +0500

    add models: User, Listing, Category, Commentary

--- PROJECTS/Ebay/commerce/auctions/admin.py
+++ PROJECTS/Ebay/commerce/auctions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "password")
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("name", "cost", "user_winner")
+
+class CommentaryAdmin(admin.ModelAdmin):
+    list_display = ("user", "commentary")
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Commentary, CommentaryAdmin)
+admin.site.register(Category)
--- PROJECTS/Ebay/commerce/auctions/models.py
+++ PROJECTS/Ebay/commerce/auctions/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+    
+    def __str__(self):
+        return self.name
+
+
+class Listing(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=500)
+    cost = models.FloatField()
+
+    image_url = models.CharField(max_length=100, blank=True)
+    user_winner = models.CharField(max_length=150, blank=True)
+    category_names = models.ManyToManyField(Category, blank=True, related_name="category")
+
+    def __str__(self):
+        return f"{self.name}: price - {self.cost}"
+    
+
+class Commentary(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commentary_user")
+    commentary = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.user}: {self.commentary}"
